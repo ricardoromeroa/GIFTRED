91a08b1f89b75b282b0a88e7b2f455d19780b273
Jesus Abraham Martinez Chavez <43384034+jabraham0301@users.noreply.github.com>
2020-06-07 16:34:58 -0500

    add negocio y linkeado de direccion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MainContainer from './containers/MainContainer/MainContainer';
-
+import DirectorioContainer from './containers/DirectorioContainer/DirectorioContainer';
+import Negocio from './components/Negocio/Negocio';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={MainContainer} />
+        <Route exact path="/negocio-register" component={Negocio}></Route>
+        <Route exact path="/directorio" component={DirectorioContainer} />
       </Switch>
     </BrowserRouter>
   );
--- src/components/LandingMain/LandingMain.js
+++ src/components/LandingMain/LandingMain.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './LandingMain.css'
+import {Link} from 'react-router-dom'
 
 const LandingMain = () => {
     return (
@@ -12,7 +13,7 @@ const LandingMain = () => {
                             <h3 className="lm-h3 mb-5">GiftCards para apoyar a pequeños negocios</h3>
                             <h5 className="lm-h5 mb-5">Con la situación económica actual, buscamos maneras de ayudar a los negocios locales. Con GIFTRED cualquier negocio se puede registrar para ofrecer Gift Cards Electronicas y cualquiera puede comprarlas para apoyar</h5>
                             <a href="/directorio" className="btn btn-light btn-xl js-scroll-trigger">Compra GiftCard</a>
-                            <a href="/directorio" className="btn btn-light btn-xl js-scroll-trigger">Registra Negocio</a>
+                            <Link to="/negocio-register" className="btn btn-light btn-xl js-scroll-trigger">Registra Negocio</Link>
                         </div>
                     </div>
                 </div>
