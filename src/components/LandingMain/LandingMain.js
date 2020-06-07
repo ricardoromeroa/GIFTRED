87a84f8e282717581d1aba647287ec7e25a1db5d
Jesus Abraham Martinez Chavez <43384034+jabraham0301@users.noreply.github.com>
2020-06-07 17:16:15 -0500

    error

--- src/components/LandingMain/LandingMain.js
+++ src/components/LandingMain/LandingMain.js
@@ -14,7 +14,7 @@ const LandingMain = () => {
             <h3 className="lm-h3 mb-5 absolute-h3">GiftCards para apoyar a pequeños negocios</h3>
             <h5 className="lm-h5 mb-5 absolute-h5">Con la situación económica actual, buscamos maneras de ayudar a los negocios locales. Con GIFTRED cualquier negocio se puede registrar para ofrecer Gift Cards Electronicas y cualquiera puede comprarlas para apoyar</h5>
             <a href="/directorio" className="btn btn-light btn-xl js-scroll-trigger absolute-button-left">Compra GiftCard</a>
-            <a href="/directorio" className="btn btn-light btn-xl js-scroll-trigger absolute-button-right">Registra Negocio</a>
+            <a href="/negocio-register" className="btn btn-light btn-xl js-scroll-trigger absolute-button-right">Registra Negocio</a>
         </header>
       </React.Fragment>
     )
