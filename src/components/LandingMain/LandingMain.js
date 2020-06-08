27c868a65d2069391c1d24ba31f7a8d543314b94
Jesus Abraham Martinez Chavez <43384034+jabraham0301@users.noreply.github.com>
2020-06-08 18:56:04 -0500

    corregir mi branch landing

--- src/components/LandingMain/LandingMain.js
+++ src/components/LandingMain/LandingMain.js
@@ -1,24 +1,22 @@
 import React from 'react'
 import './LandingMain.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const LandingMain = () => {
     return (
+      <React.Fragment>
         <header className="masthead">
-            <div className="container h-100">
+            <div className="h-100 bg-img">
                 <div className="row h-100">
-                    <div className="col-lg-7 my-auto">
-                        <div className="header-content mx-auto">
-                            <h1 className="lm-h1 mb-5">Apoya a los negocios de tu localidad</h1>
-                            <h3 className="lm-h3 mb-5">GiftCards para apoyar a pequeños negocios</h3>
-                            <h5 className="lm-h5 mb-5">Con la situación económica actual, buscamos maneras de ayudar a los negocios locales. Con GIFTRED cualquier negocio se puede registrar para ofrecer Gift Cards Electronicas y cualquiera puede comprarlas para apoyar</h5>
-                            <a href="/directorio" className="btn btn-light btn-xl js-scroll-trigger">Compra GiftCard</a>
-                            <Link to="/negocio-register" className="btn btn-light btn-xl js-scroll-trigger">Registra Negocio</Link>
-                        </div>
-                    </div>
                 </div>
             </div>
+            <h1 className="lm-h1 mb-5 absolute-h1">Apoya a los negocios de tu localidad</h1>
+            <h3 className="lm-h3 mb-5 absolute-h3">GiftCards para apoyar a pequeños negocios</h3>
+            <h5 className="lm-h5 mb-5 absolute-h5">Con la situación económica actual, buscamos maneras de ayudar a los negocios locales. Con GIFTRED cualquier negocio se puede registrar para ofrecer Gift Cards Electronicas y cualquiera puede comprarlas para apoyar</h5>
+            <a href="/directorio" className="btn btn-light btn-xl js-scroll-trigger absolute-button-left">Compra GiftCard</a>
+            <a href="/negocio-register" className="btn btn-light btn-xl js-scroll-trigger absolute-button-right">Registra Negocio</a>
         </header>
+      </React.Fragment>
     )
 }
 
