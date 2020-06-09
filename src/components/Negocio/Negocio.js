a2f8603f20a514a4ab0e39f453e7ca9a3db7a8e9
Jesus Abraham Martinez Chavez <43384034+jabraham0301@users.noreply.github.com>
2020-06-09 18:26:33 -0500

    re-styling form negocio
    volvi a agregar estilos css a el formulario, falta la seccion de botones

--- src/components/Negocio/Negocio.js
+++ src/components/Negocio/Negocio.js
@@ -1,7 +1,10 @@
-import React,{useState}from 'react';
+import React from 'react';
 import NavBar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 import NegocioStyles from './Negocio.css';
+
+
+
 
 const Negocio=()=>{
 
@@ -31,109 +34,114 @@ const Negocio=()=>{
         <React.Fragment>
             <NavBar/>
             <form>
-
                 <header>
-                    <h4>Favor de llenar el siguiente formulario</h4>
+                    <nav id="headerForm"class="navbar navbar-light bg-light">
+                        <span id="titleRegistro"class="navbar-brand mb-0 h1">Registro</span>
+                    </nav>
                 </header>
-                
-                <h5>Informacion Del Negocio</h5>
-                <div class="row">
-                    <div class="col">
-                        <label for="nombreNegocioInput">Nombre del Negocio</label>
-                        <input type="text" class="form-control" id="nombreNegocioInput" placeholder="Tacos Chuchita"/>
+                <div id="formWraper">
+                    <div id="negocioSection">
+                        <h5 class="h5">Informacion Del Negocio</h5>
+                        <div class="row">
+                            <div class="col-sm">
+                                <label for="nombreNegocioInput">Nombre</label>
+                                <input type="text" class="form-control" id="nombreNegocioInput" placeholder="Tacos Chuchita"/>
+                            </div>
+                            <div class="col-sm">
+                                <label for="giroNegocioInput">Giro</label>
+                                <input type="text" class="form-control" id="giroNegocioInput" placeholder="Comida rapida"/>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm">
+                                <label for="telefonoNegocioInput">Telefono</label>
+                                <input type="tel" class="form-control" id="telefonoNegocioInput" placeholder="1112223343"/>
+                            </div>
+                        </div>
+                        <div class="divider"></div>
                     </div>
-                    <div class="col">
-                        <label for="giroNegocioInput">Giro</label>
-                        <input type="text" class="form-control" id="giroNegocioInput" placeholder="Comida rapida"/>
+                    <div id="giftcardConfigSection">
+                        <h5 class="h5">Configuraciones para Giftcards</h5>
+                        <div class="row">
+                            <div class="col-sm">
+                                <label for="plazoVencimientoInput">Plazo de Vencimiento</label>
+                                <input type="date" class="form-control" id="plazoVencimientoInput"/>
+                            </div>
+                        </div>
+                        <div class="divider"></div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <label for="telefonoNegocioInput">Telefono Negocio</label>
-                        <input type="tel" class="form-control" id="telefonoNegocioInput" placeholder="1112223343"/>
+                    <div id="direccionSection">
+                        <h5 class="h5">Direccion</h5>
+                        <div class="row">
+                            <div class="col-sm">
+                                <label for="calleInput">Calle</label>
+                                <input type="text" class="form-control" id="calleInput" placeholder="Moctezuma"/>
+                            </div>
+                            <div class="col-sm">
+                                <label for="ciudadInput">Ciudad</label>
+                                <input type="text" class="form-control" id="ciudadInput" placeholder="monterrey"/>
+                            </div>    
+                        </div>
+                        <div class="row">
+                            <div class="col-sm">
+                                <label for="estadoInput">Estado</label>
+                                <input type="text" class="form-control" id="estadoInput" placeholder="Nuevo Leon"/>
+                            </div>
+                            <div class="col-sm">
+                                <label for="cpInput">Codigo Postal</label>
+                                <input type="number" class="form-control" id="cpInput"/>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm">
+                                <label for="paisSelect">Pais</label>
+                                <select class="form-control" id="paisSelect">
+                                    <option>Mexico</option>
+                                    <option>Colombia</option>
+                                    <option>Canada</option>
+                                    <option>Peru</option>
+                                    <option>Guatemala</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="divider"></div>
                     </div>
-                </div>
-                
-                <div class="dropdown-divider"></div>
-                
-                <h5>Configuraciones para Giftcards</h5>
-                <div class="row">
-                    <div class="col">
-                        <label for="plazoVencimientoInput">Plazo de Vencimiento</label>
-                        <input type="date" class="form-control" id="plazoVencimientoInput"/>
-                    </div>
-                </div>
-                
-                <div class="dropdown-divider"></div>
-                
-                <h5>Direccion</h5>
-                <div class="row">
-                    <div class="col">
-                        <label for="calleInput">Calle</label>
-                        <input type="text" class="form-control" id="calleInput" placeholder="Moctezuma"/>
-                    </div>
-                    <div class="col">
-                        <label for="ciudadInput">Ciudad</label>
-                        <input type="text" class="form-control" id="ciudadInput" placeholder="monterrey"/>
-                    </div>    
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <label for="estadoInput">Estado</label>
-                        <input type="text" class="form-control" id="estadoInput" placeholder="Nuevo Leon"/>
-                    </div>
-                    <div class="col">
-                        <label for="cpInput">Codigo Postal</label>
-                        <input type="number" class="form-control" id="cpInput"/>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col">
-                        <label for="paisSelect">Pais</label>
-                        <select class="form-control" id="paisSelect">
-                            <option>Mexico</option>
-                            <option>Colombia</option>
-                            <option>Canada</option>
-                            <option>Peru</option>
-                            <option>Guatemala</option>
-                        </select>
-                    </div>
-                </div>
+                    <div id="responsableSection">
+                        <h5 class="h5">Responsable</h5>
+                        <div class="row">
+                            <div class="col-sm">
+                                <label for="nombreInput">Nombre</label>
+                                <input type="text" class="form-control" id="nombreInput" placeholder="Hector Gabriel" />
+                            </div>
+                            <div class="col-sm">
+                                <label for="correoResponsableInput">Correo</label>
+                                <input type="email" class="form-control" id="correoResponsableInput" placeholder="name@example.com"/>
+                            </div>
+                            <div class="col-sm">
+                                <label for="telefonoResponsableInput">Telefono</label>
+                                <input type="email" class="form-control" id="telefonoResponsableInput" placeholder="35416321"/>
+                            </div>
+                        </div>
 
-                <div class="dropdown-divider"></div>
-                
-                <h5>Responsable</h5>
-                <div class="row">
-                    <div class="col">
-                        <label for="nombreInput">Nombre</label>
-                        <input type="text" class="form-control" id="nombreInput" placeholder="Hector Gabriel" />
+                        <div class="divider"></div>
                     </div>
-                    <div class="col">
-                        <label for="correoResponsableInput">Correo</label>
-                        <input type="email" class="form-control" id="correoResponsableInput" placeholder="name@example.com"/>
+                    <div id="cuentaSection">
+                        <h5 class="h5">Informacion de Cuenta</h5>
+                        <div class="row">
+                            <div class="col-sm">
+                                <label for="passwordInput">Contraseña</label>
+                                <input type="password" class="form-control" id="passwordInput" />
+                            </div>
+                            <div class="col-sm">
+                                <label for="correoNegocioInput">Correo del Negocio</label>
+                                <input type="email" class="form-control" id="correoNegocioInput" placeholder="name@example.com"/>
+                            </div>
+                        </div>
+
+                        <div class="divider"></div>
                     </div>
-                    <div class="col">
-                        <label for="telefonoResponsableInput">Telefono Responsable</label>
-                        <input type="email" class="form-control" id="telefonoResponsableInput" placeholder="35416321"/>
-                    </div>
+                    <button type="button" class="btn btn-danger" onClick={submitNegocio}>Registrarse</button>
                 </div>
-
-                <div class="dropdown-divider"></div>
-
-                <h5>Informacion de Cuenta</h5>
-                <div class="row">
-                    <div class="col">
-                        <label for="passwordInput">Contraseña</label>
-                        <input type="password" class="form-control" id="passwordInput" />
-                    </div>
-                    <div class="col">
-                        <label for="correoNegocioInput">Correo del Negocio</label>
-                        <input type="email" class="form-control" id="correoNegocioInput" placeholder="name@example.com"/>
-                    </div>
-                </div>
-
-                <div class="dropdown-divider"></div>
-                <button type="button" class="btn btn-primary" onClick={submitNegocio} data-toggle="modal" data-target="#exampleModal">Registrarse</button>
             </form>
             <Footer/>
         </React.Fragment>
