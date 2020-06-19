4b7a195b8fe6290aa0353f7bb321322fa56ca98e
Ricardor <ricardoromeroavila@gmail.com>
2020-06-19 07:58:43 -0700

    updates a landing page

--- src/components/HelpSection/HelpSection.js
+++ src/components/HelpSection/HelpSection.js
@@ -5,10 +5,10 @@ const HelpSection = () => {
     return (
             <section className="section">
                 <div className="sectioncard text-center">
-                    <div className="card-body">
+                    <div className="card-body1">
                         <h2 className="section-title">¿Listo para apoyar a tu comunidad?</h2>
                         <h4 className="section-text">Busca negocios participantes por giro o ubicación para apoyarlos. Tambíen puedes invitar a tu negocio local favorito a unirse a GIFTRED. Ayudémonos entre todos.</h4>
-                        <a href="/" className="btn btn-light section-button">Ir al Directorio</a>
+                        <a href="/" className="btn btn-primary section-button">Directorio</a>
                     </div>
                 </div>
             </section>
